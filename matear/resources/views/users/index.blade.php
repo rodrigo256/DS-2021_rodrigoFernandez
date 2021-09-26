c9541d43586b6603b29597f11b95ed2adb7e203f
Rodrigo <rodrigo_efernandez@outlook.com>
2021-09-26 03:43:01 -0300

    finish custom profile

--- matear/resources/views/users/index.blade.php
+++ matear/resources/views/users/index.blade.php
@@ -38,14 +38,16 @@
                             <div class=" mb-3">
                             <div class=" row gutters">
                                 <div class="col-md-12">
-                                    <div class="card" style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
-                                                                                border-radius: 15px; width: 100%;">
+                                    <div class="card"
+                                        style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
+                                                                                                        border-radius: 15px; width: 100%;">
                                         <div class="card-body">
                                             <div class="form-group row">
                                                 <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
                                                 <div class="col-sm-10">
-                                                    <input type="email" class="form-control" style="border-radius: 15px"
-                                                        id="email" placeholder="{{ auth()->user()->email }}" disabled>
+                                                    <input type="email" class="form-control"
+                                                        style="border: 0; background-color: white;" id="email"
+                                                        placeholder="{{ auth()->user()->email }}" disabled>
                                                 </div>
                                             </div>
                                         </div>
@@ -54,58 +56,73 @@
                             </div>
                         </div>
                         <div class="mb-3">
-                                <h4 class=" mb-2 text-dark">Datos personales</h4>
+                            <h4 class=" mb-2 text-dark">Datos personales</h4>
                         </div>
                         <div class="mb-3">
-                                <div class=" row gutters">
+                            <div class=" row gutters">
+                                <div class="col-md-12">
+                                    <div class="card"
+                                        style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
+                                                                                                border-radius: 15px; width: 100%;">
+                                        <div class="card-body">
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label" for="phone">Nombre</label>
+                                                <div class="col-sm-10">
+                                                    <input style="border: 0; background-color: white;" type="text"
+                                                        class="form-control" id="name"
+                                                        placeholder="{{ auth()->user()->name }}" disabled>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label" for="website">Apellido</label>
+                                                <div class="col-sm-10">
+                                                    <input style="border: 0; background-color: white;" type="text"
+                                                        class="form-control" id="surname"
+                                                        placeholder="{{ auth()->user()->surname }}" disabled>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label" for="website">Documento</label>
+                                                <div class="col-sm-10">
+                                                    <input style="border: 0; background-color: white;" type="text"
+                                                        class="form-control" id="surname"
+                                                        placeholder="DNI {{ auth()->user()->dni }}" disabled>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label" for="website">Teléfono</label>
+                                                <div class="col-sm-10">
+                                                    <input style=" border: 0; background-color: white;" type="text"
+                                                        class="form-control" id="surname"
+                                                        placeholder="{{ auth()->user()->phone }}" disabled>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label" for="website">Dirección</label>
+                                                <div class="col-sm-10">
+                                                    <input style=" border: 0; background-color: white;" type="text"
+                                                        class="form-control" id="surname"
+                                                        placeholder="{{ auth()->user()->address }}" disabled>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <h4 class=" mb-2 text-dark">Tarjetas</h4>
+                    </div>
+                    <div class="mb-3">
+                        <div class=" row gutters">
                             <div class="col-md-12">
-                                <div class="card" style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
-                                                                        border-radius: 15px; width: 100%;">
+                                <div class="card"
+                                    style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
+                                                                                                border-radius: 15px; width: 100%;">
                                     <div class="card-body">
                                         <div class="form-group">
-                                            <div class="row">
-                                                <div class="col-12">
-                                                    <div class="form-group">
-                                                        <label for="phone">Nombre</label>
-                                                        <input style="border-radius: 15px" type="text"
-                                                            class="form-control" id="name"
-                                                            placeholder="{{ auth()->user()->name }}" disabled>
-                                                    </div>
-                                                </div>
-                                                <div class="col-12">
-                                                    <div class="form-group">
-                                                        <label for="website">Apellido</label>
-                                                        <input style="border-radius: 15px" type="text"
-                                                            class="form-control" id="surname"
-                                                            placeholder="{{ auth()->user()->surname }}" disabled>
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                                    <div class="form-group">
-                                                        <label for="website">DNI</label>
-                                                        <input style="border-radius: 15px" type="number"
-                                                            class="form-control" id="dni"
-                                                            placeholder="{{ auth()->user()->dni }}" disabled>
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                                    <div class="form-group">
-                                                        <label for="website">Teléfono</label>
-                                                        <input style="border-radius: 15px" type="number"
-                                                            class="form-control" id="phone"
-                                                            placeholder="{{ auth()->user()->phone }}" disabled>
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                                    <div class="form-group">
-                                                        <label for="website">Dirección</label>
-                                                        <input style="border-radius: 15px" type="text"
-                                                            class="form-control" id="address"
-                                                            placeholder="{{ auth()->user()->address }}" disabled>
-                                                    </div>
-                                                </div>
 
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -113,119 +130,26 @@
                         </div>
                     </div>
                     <div class="mb-3">
-                                <h4 class=" mb-2 text-dark">Tarjetas</h4>
-                    </div>
-                    <div class="mb-3">
-                                <div class=" row gutters">
-                        <div class="col-md-12">
-                            <div class="card" style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
-                                                                        border-radius: 15px; width: 100%;">
-                                <div class="card-body">
-                                    <div class="form-group">
-
-                                    </div>
+                        <div class="row gutters" style="padding: 20px">
+                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                <div class="text-right" style="display: flex; justify-content: space-between">
+                                    <a href="{{ url('/user/' . auth()->user()->id . '/edit') }}" type="button"
+                                        class="btn btn-success" style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
+                                                            border-radius: 15px;">Editar datos</a>
+                                    <form action="{{ url('/user/' . auth()->user()->id) }}" method="post">
+                                        @csrf
+                                        {{ method_field('DELETE') }}
+                                        <input style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
+                                                            border-radius: 15px;" type="submit" class="btn btn-danger"
+                                            onclick="return confirm('¿Quieres borrar tu cuenta?')" value="Eliminar cuenta">
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="mb-3">
-                    <div class="row gutters" style="padding: 20px">
-                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                            <div class="text-right" style="display: flex; justify-content: space-between">
-                                <a href="{{ url('/user/' . auth()->user()->id . '/edit') }}" type="button"
-                                    class="btn btn-success" style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
-                                    border-radius: 15px;">Editar datos</a>
-                                <form action="{{ url('/user/' . auth()->user()->id) }}" method="post">
-                                    @csrf
-                                    {{ method_field('DELETE') }}
-                                    <input style="box-shadow: 0 5px 20px rgb(0 0 0 / 0%), 0 5px 10px rgb(0 0 0 / 10%);
-                                    border-radius: 15px;" type="submit" class="btn btn-danger"
-                                        onclick="return confirm('¿Quieres borrar tu cuenta?')" value="Eliminar cuenta">
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
             </div>
-
-
-            {{-- aca <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                    <h6 class="mb-2 text-primary">Mis datos</h6>
-
-                                </div>
-                                <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9  col-12">
-                                    <div class="form-group">
-                                        <label for="fullName">Email</label>
-                                        <input type="email" class="form-control" id="email"
-                                            placeholder="{{ auth()->user()->email }}" disabled>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-12">
-                                    <div class="form-group">
-                                        <label for="eMail">Contraseña</label>
-                                        <div class="input-group">
-                        
-                                            <div class="input-group-prepend">
-                                                <div class="input-group-text" id="btnGroupAddon"> <a
-                                                        href="{{ route('password.request') }}">Cambiar contraseña</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row gutters">
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                    <h6 class="mb-2 text-primary">Datos personales</h6>
-                                </div>
-                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                    <div class="form-group">
-                                        <label for="phone">Nombre</label>
-                                        <input type="text" class="form-control" id="name"
-                                            placeholder="{{ auth()->user()->name }}" disabled>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                    <div class="form-group">
-                                        <label for="website">Apellido</label>
-                                        <input type="text" class="form-control" id="surname"
-                                            placeholder="{{ auth()->user()->surname }}" disabled>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                    <div class="form-group">
-                                        <label for="website">DNI</label>
-                                        <input type="number" class="form-control" id="dni"
-                                            placeholder="{{ auth()->user()->dni }}" disabled>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                    <div class="form-group">
-                                        <label for="website">Telefono</label>
-                                        <input type="number" class="form-control" id="phone"
-                                            placeholder="{{ auth()->user()->phone }}" disabled>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                    <div class="form-group">
-                                        <label for="website">Direccion</label>
-                                        <input type="text" class="form-control" id="address"
-                                            placeholder="{{ auth()->user()->address }}" disabled>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row gutters">
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                    <h6 class="mb-2 text-primary">Tarjetas</h6>
-                                </div>
-                            </div> --}}
-
         </div>
-    </div>
-    </div>
-    </div>
     </div>
     </div>
 @endsection
