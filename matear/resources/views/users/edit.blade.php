01d14d1dd301807d20926660f362700b0f7b2e61
Rodrigo <rodrigo_efernandez@outlook.com>
2021-10-30 15:38:28 -0300

    new styles from edit dates

--- matear/resources/views/users/edit.blade.php
+++ matear/resources/views/users/edit.blade.php
@@ -3,9 +3,13 @@
 @section('content')
     <div class="container" style="margin-top: 80px;">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Editá tus datos') }}</div>
+            <div class="col-md-6" style="display: flex; justify-content:center">
+                <div class="" style=" box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+                border-radius: 15px; width: 80%;">
+                <div style="padding: 20px 20px 0px; text-align:center;">
+                    <h3>Editá tus datos</h3>
+                </div>
+                    
                     <div class="card-body">
                         <form method="post" action="{{ url('/user/' . $user->id) }}" autocomplete="off">
                             @csrf
